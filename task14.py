d5fea46b00463390a9e9d607ad0df6c66de9e5c1
Rosislav <rostislavnosovskij467@gmail.com>
2023-05-24 13:12:04 +0300

    Домашнее задание ко второму семинару

---
+++ task14.py
@@ -0,0 +1,10 @@
+# Задача 14: Требуется вывести все целые степени двойки 
+# (т.е. числа вида 2k), не превосходящие числа N.
+
+num = int(input('Введите число'))
+derdreeof2 = 1
+while derdreeof2 <=num:
+    print(derdreeof2)
+    derdreeof2 *=2
+    
+
